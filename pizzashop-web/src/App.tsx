b2fa11575f0acc9187c8a4340305e86cff36cd90
Rian Evangelista <83549817+rianevangelista@users.noreply.github.com>
2024-12-06 01:12:18 +0000

    login-page

--- pizzashop-web/src/App.tsx
+++ pizzashop-web/src/App.tsx
@@ -1,10 +1,14 @@
-import { Button } from './components/ui/button'
+import { RouterProvider } from 'react-router-dom'
+
 import './global.css'
+import { router } from './routes'
+import { HelmetProvider, Helmet } from 'react-helmet-async'
 
 export function App() {
   return (
-    <div className='flex justify-center items-center h-screen'>
-      <Button>Enviar</Button>
-    </div>
+    <HelmetProvider>
+      <Helmet titleTemplate='%s | Pizza Shop'/>
+      <RouterProvider router={router}/>
+    </HelmetProvider>
   )
 }
