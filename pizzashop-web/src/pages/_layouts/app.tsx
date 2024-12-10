c188d828c9a59e9caf2059ac954f8e6825f0a390
Rian Evangelista <83549817+rianevangelista@users.noreply.github.com>
2024-12-10 02:30:57 +0000

    home header

--- pizzashop-web/src/pages/_layouts/app.tsx
+++ pizzashop-web/src/pages/_layouts/app.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 export function AppLayout() {
     return (
-        <div>
+        <div className="flex min-h-screen flex-col">
             <h1>Header</h1>
 
             <div>
