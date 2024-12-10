c0e0f34cd8c33fa0fac2296b0c407cfb6eeb7a9a
Rian Evangelista <83549817+rianevangelista@users.noreply.github.com>
2024-12-10 02:12:55 +0000

    labels added

--- pizzashop-web/src/pages/auth/sign-up.tsx
+++ pizzashop-web/src/pages/auth/sign-up.tsx
@@ -2,19 +2,25 @@ import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
 import { toast } from 'sonner'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 
 const signUpForm = z.object({
+    restaurantName: z.string(),
+    managerName: z.string(),
+    phone: z.string(),
     email: z.string().email(),
 })
 
 type SignUpForm = z.infer<typeof signUpForm>
 
 export function SignUp() {
+
+    const navigate = useNavigate()
+
     const { 
         register, 
         handleSubmit, 
@@ -25,14 +31,14 @@ export function SignUp() {
         try {
             await new Promise((resolve) => setTimeout(resolve, 2000))
     
-            toast.success('Enviamos um link de autenticação para o seu email.', {
+            toast.success('Restaurante cadastrado com sucesso!', {
                 action: {
-                    label: 'Reenviar',
-                    onClick: () => handleSignUp(data),
+                    label: 'Login',
+                    onClick: () => navigate('/sign-in'),
                 }
             })
         } catch {
-            toast.error('Credenciais inválidas.')
+            toast.error('Erro ao cadastrar restaurante.')
         }
     }
 
@@ -57,13 +63,55 @@ export function SignUp() {
 
                     <form onSubmit={handleSubmit(handleSignUp)} className='space-y-4'>
                         <div className='space-y-2'>
+                            <Label htmlFor='restaurantName'>Nome do estabelecimento</Label>
+                            <Input 
+                                id='restaurantName' 
+                                type='text' 
+                                {...register('restaurantName')} 
+                            />
+                        </div>
+
+                        <div className='space-y-2'>
+                            <Label htmlFor='managerName'>Seu nome</Label>
+                            <Input 
+                                id='managerName' 
+                                type='text' 
+                                {...register('managerName')} 
+                            />
+                        </div>
+
+                        <div className='space-y-2'>
                             <Label htmlFor='email'>Seu e-mail</Label>
-                            <Input id='email' type='email' {...register('email')} />
+                            <Input 
+                                id='email' 
+                                type='email' 
+                                {...register('email')} 
+                            />
+                        </div>
+
+                        <div className='space-y-2'>
+                            <Label htmlFor='phone'>Seu celular</Label>
+                            <Input 
+                                id='phone' 
+                                type='tel' 
+                                {...register('phone')} 
+                            />
                         </div>
 
                         <Button disabled={isSubmitting} className='w-full' type='submit'>
                             Finalizar cadastro
                         </Button>
+
+                        <p className='px-6 text-center text-sm leading-relaxed text-muted-foreground'>
+                            Ao continuar, você concorda com os nossos{' '} 
+                            <a className='underline underline-offset-4' href=''>
+                                Termos de serviço
+                            </a>{' '} 
+                            e{' '} 
+                            <a className='underline underline-offset-4' href=''>
+                                Política de privacidade
+                            </a>.
+                        </p>
                     </form>
                 </div>
             </div>
