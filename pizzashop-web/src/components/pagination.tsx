5a4bb75cdaf7f56f20dcb04e61fdc18ca37de4d9
Rian Evangelista <83549817+rianevangelista@users.noreply.github.com>
2024-12-14 16:48:01 +0000

    paginacao

--- pizzashop-web/src/components/pagination.tsx
+++ pizzashop-web/src/components/pagination.tsx
@@ -1,4 +1,4 @@
-import { ChevronLeft } from "lucide-react"
+import { ChevronLeft, ChevronRight, ChevronsLeft, ChevronsRight } from "lucide-react"
 import { Button } from "./ui/button"
 
 export interface PaginationProps {
@@ -26,8 +26,20 @@ export function Pagination({
                 </div>
                 <div className="flex items-center gap-2">
                     <Button variant="outline" className="h-8 w-8 p-8">
-                        <ChevronLeft className="h-4 w-4" />
+                        <ChevronsLeft className="h-4 w-4" />
                         <span className="sr-only">Primeira página</span>
+                    </Button>
+                    <Button variant="outline" className="h-8 w-8 p-8">
+                        <ChevronLeft className="h-4 w-4" />
+                        <span className="sr-only">Página anterior</span>
+                    </Button>
+                    <Button variant="outline" className="h-8 w-8 p-8">
+                        <ChevronRight className="h-4 w-4" />
+                        <span className="sr-only">Próxima página</span>
+                    </Button>
+                    <Button variant="outline" className="h-8 w-8 p-8">
+                        <ChevronsRight className="h-4 w-4" />
+                        <span className="sr-only">Última página</span>
                     </Button>
                 </div>
             </div>
